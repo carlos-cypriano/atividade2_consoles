c237ed6a19943dc0d5fc730e57f13c635ce5830d
carlos-cypriano <81194143+carlos-cypriano@users.noreply.github.com>
2021-04-08 16:35:39 -0300

    IMPLEMENTANDO MULTITHREADS EM CORRIDA DE GRILO
    Foi criado o método JumpingCricket() para aplicar o numero total de pulos e mecânica de deslocamento;
    Também foi criado o método printRacing() para imprimir a corrida;
    Foi criado o método placingCricket(0 para incrementar a colocação e imprimir a mesma;
    Foi criado o método run() que implementa a interface Runnable realizando um loop para que os métodos jumpingCricket() e printRacing() fossem chamados e ao final chamando o método de classificação placingCricket();
    Por fim na classe RacingCricket foram definidos a quantidade de grilos e a distancia total a ser percorrida.

---
+++ JumpingCricket.java
@@ -0,0 +1,48 @@
+/**
+ * JumpingCricket
+ */
+public class JumpingCricket extends Thread{
+
+    String name;
+    int jumpDistance = 0;
+    int totalDistance;
+    int jumps = 0;
+    int jump = 0;
+    static int placing = 0;
+    final static int MAX_JUMP = 40;
+
+
+    JumpingCricket( String name, int totalDistance){
+
+        this.name = name;
+        this.totalDistance = totalDistance;
+    }
+
+    public void printRacing(){
+        
+        System.out.println("O " + name + " pulou " + jump + " cm e já percorreu a distância de " + jumpDistance + " cm.");
+    }
+
+    public void jumpingCricket(){
+        jumps++;
+        jump = (int) (Math.random() * MAX_JUMP);
+        jumpDistance += jump;
+        if(jumpDistance > totalDistance){
+            jumpDistance = totalDistance;
+        }
+    }
+
+    public void placingCricket(){
+        placing++;
+        System.out.println(name + " foi o " + placing + " º colocado com " + jumps + " pulos.");
+    }
+ 
+    public void run(){
+
+        while (jumpDistance < totalDistance){
+            jumpingCricket();
+            printRacing();
+        }
+        placingCricket();
+    }
+}
